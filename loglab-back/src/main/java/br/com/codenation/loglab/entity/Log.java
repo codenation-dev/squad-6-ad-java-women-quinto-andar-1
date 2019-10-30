d69788861881c9bde2c0a52a9bf970cea18a6073
Marcella Farias <marcellafc@live.com>
2019-10-29 22:16:35 -0300

    merge to develop

--- loglab-back/src/main/java/br/com/codenation/loglab/entity/Log.java
+++ loglab-back/src/main/java/br/com/codenation/loglab/entity/Log.java
@@ -18,119 +18,116 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class Log {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@Column(name = "level")
-	@NotNull
-	private String levelType;
+    @Column(name = "level")
+    @NotNull
+    private String levelType;
 
-	@Column(name = "title")
-	@NotNull
-	private String titleError;
+    @Column(name = "title")
+    @NotNull
+    private String titleError;
 
-	@ManyToOne
-	private User userId;
+    @ManyToOne
+    private User user;
 
-	private String details;
+    private String details;
 
-	@CreatedDate
-	@NotNull
-	private LocalDateTime createdAt;
+    @CreatedDate
+    @NotNull
+    private LocalDateTime createdAt;
 
-	@Column(name = "frequency")
-	private Long quantity;
+    @Column(name = "frequency")
+    private Long quantity;
 
-	private Boolean filed;
+    private Boolean filed;
 
-	private String environment;
+    private String environment;
 
-	@ManyToOne
-	private User userSourceIp;
+    public Integer getId() {
+        return id;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Log() {
+    }
 
-	public Log() {
-	}
+    public Log(Integer id, @NotNull String levelType, @NotNull String titleError, User userId, String details,
+               @NotNull LocalDateTime createdAt, Long quantity, Boolean filed, String environment, User userSourceIp) {
+        this.id = id;
+        this.levelType = levelType;
+        this.titleError = titleError;
+        this.userId = userId;
+        this.details = details;
+        this.createdAt = createdAt;
+        this.quantity = quantity;
+        this.filed = filed;
+        this.environment = environment;
+        this.userSourceIp = userSourceIp;
+    }
 
-	public Log(Integer id, @NotNull String levelType, @NotNull String titleError, User userId, String details,
-			@NotNull LocalDateTime createdAt, Long quantity, Boolean filed, String environment, User userSourceIp) {
-		this.id = id;
-		this.levelType = levelType;
-		this.titleError = titleError;
-		this.userId = userId;
-		this.details = details;
-		this.createdAt = createdAt;
-		this.quantity = quantity;
-		this.filed = filed;
-		this.environment = environment;
-		this.userSourceIp = userSourceIp;
-	}
+    public String getLevelType() {
+        return levelType;
+    }
 
-	public String getLevelType() {
-		return levelType;
-	}
+    public void setLevelType(String levelType) {
+        this.levelType = levelType;
+    }
 
-	public void setLevelType(String levelType) {
-		this.levelType = levelType;
-	}
+    public String getTitleError() {
+        return titleError;
+    }
 
-	public String getTitleError() {
-		return titleError;
-	}
+    public void setTitleError(String titleError) {
+        this.titleError = titleError;
+    }
 
-	public void setTitleError(String titleError) {
-		this.titleError = titleError;
-	}
+    public User getUserId() {
+        return userId;
+    }
 
-	public User getUserId() {
-		return userId;
-	}
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
 
-	public void setUserId(User userId) {
-		this.userId = userId;
-	}
+    public String getDetails() {
+        return details;
+    }
 
-	public String getDetails() {
-		return details;
-	}
+    public void setDetails(String details) {
+        this.details = details;
+    }
 
-	public void setDetails(String details) {
-		this.details = details;
-	}
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
 
-	public LocalDateTime getCreatedAt() {
-		return createdAt;
-	}
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 
-	public void setCreatedAt(LocalDateTime createdAt) {
-		this.createdAt = createdAt;
-	}
+    public Long getQuantity() {
+        return quantity;
+    }
 
-	public Long getQuantity() {
-		return quantity;
-	}
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
 
-	public void setQuantity(Long quantity) {
-		this.quantity = quantity;
-	}
+    public Boolean getFiled() {
+        return filed;
+    }
 
-	public Boolean getFiled() {
-		return filed;
-	}
+    public void setFiled(Boolean filed) {
+        this.filed = filed;
+    }
 
-	public void setFiled(Boolean filed) {
-		this.filed = filed;
-	}
+    public String getEnvironment() {
+        return environment;
+    }
 
-	public String getEnvironment() {
-		return environment;
-	}
-
-	public User getUserSourceIp() {
-		return userSourceIp;
-	}
+    public User getUserSourceIp() {
+        return userSourceIp;
+    }
 }
