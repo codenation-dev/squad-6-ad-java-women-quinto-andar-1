8401d0ed14d1c2b8719121f7606e1104f901f6c2
Patricia Clares <dev.patricia.c@gmail.com>
2019-11-14 18:46:55 -0300

    adicionando metodo handleMethodArgumentNotValid

---
+++ loglab-back/src/main/java/br/com/codenation/loglab/excecoes/RestExceptionHandler.java
@@ -0,0 +1,10 @@
+package br.com.codenation.loglab.excecoes;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler{
+
+	
+}
