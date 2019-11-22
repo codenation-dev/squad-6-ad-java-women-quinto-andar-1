8d529baae6a8697d42517a1152323eff775824f4
Marcella Farias <marcellafc@live.com>
2019-11-22 02:05:00 -0300

    inclusão dependencia swagger

---
+++ loglab-back/src/main/java/br/com/codenation/loglab/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package br.com.codenation.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(
+                        "br.com.codenation.spring.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Documentação da API Rest")
+                .description("\" Documentação do projeto que estamos desenvolvendo\"")
+                .version("1.0.0")
+                .contact(new Contact("Marcella Farias", "https://github.com/marcellafarias", "fcosta.marcella@gmail.com"))
+                .build();
+    }
+}
